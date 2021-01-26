bc74c458dbfd06c1245bdf8b0f860420944124de
Azalea Marcia <azaleamarcia1@gmail.com>
2021-01-26 17:11:41 -0500

    Ruby basico

---
+++ strings.rb
@@ -0,0 +1,8 @@
+a = "este es un lindo strings"
+b = "otro strings"
+#En ruby todo es casi un objeto
+puts a.length
+puts b.length
+#strings de multiples lineas 
+c = "Esto es\nstrings\nmultiples lineas"
+puts c
